873cd1285afa839123a268590d01e91fd4c55539
YourFBIAgent <nedzaddonlagic04@gmail.com>
2023-01-29 15:29:11 +0100

    Finished practice problem 3

--- src/Main.java
+++ src/Main.java
@@ -1,15 +1,12 @@
 public class Main {
     public static void main(String[] args) {
-        Desktop desktop = new Desktop("idk4", "idk5", "idk6", 5.5);
-        Laptop laptop = new Laptop("idk7", "idk8", "idk9", 60, 10.5);
+        Computer n = new Netbook("Ideapad S12", "Lenovo", "Windows", 50, 1.55);
 
-        System.out.println(desktop);
-        System.out.println(desktop.getComputerType());
-        System.out.println(desktop.calculatePortabilityScore());
-        System.out.println();
+        System.out.println(n);
+        System.out.println(n.getComputerType());
+        System.out.println(n.calculatePortabilityScore());
 
-        System.out.println(laptop);
-        System.out.println(laptop.getComputerType());
-        System.out.println(laptop.calculatePortabilityScore());
+        Laptop l = (Laptop) n;
+        System.out.println(l.calculatePortabilityScore());
     }
 }
---
+++ src/Netbook.java
@@ -0,0 +1,13 @@
+public final class Netbook extends Laptop{
+    public Netbook(String model, String manufacturer, String operatingSystem, int batteryCapacity, double weigh) {
+        super(model, manufacturer, operatingSystem, batteryCapacity, weigh);
+    }
+
+    public String getComputerType() {
+        return "netbook laptop computer";
+    }
+
+    public int calculatePortabilityScore() {
+        return 1;
+    }
+}
